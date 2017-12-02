4510dbe1e578884d0a3f4194e98b7551e1d5f0e4
MonchoG <31766417+MonchoG@users.noreply.github.com>
2017-12-02 12:52:09 +0100

    GUI for weatherstation
    Imports fixed, works when updating the database

--- JDBC/src/main/java/WeatherStationGUI.java
+++ JDBC/src/main/java/WeatherStationGUI.java
@@ -1,8 +1,11 @@
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -10,22 +13,24 @@ import javafx.scene.layout.Pane;
 
 public class WeatherStationGUI extends BorderPane {
 
-    private javafx.scene.control.TextField time = new javafx.scene.control.TextField();
-    private javafx.scene.control.TextField temperature = new javafx.scene.control.TextField();
-    private javafx.scene.control.TextField pressure = new javafx.scene.control.TextField();
-    private javafx.scene.control.TextField light = new javafx.scene.control.TextField();
+    private TextField time = new TextField();
+    private TextField temperature = new TextField();
+    private TextField pressure = new TextField();
+    private TextField light = new TextField();
 
 
-    private javafx.scene.control.Button refresh = new javafx.scene.control.Button("refresh...");
+    private Button refresh = new Button("refresh...");
 
+    DAO dbc = new DAO(DBmanager.getInstance());
 
     public WeatherStationGUI() {
         setPadding(new javafx.geometry.Insets(10, 10, 10, 10));
         setCenter(initFields());
+        setFieldData(dbc);
         setBottom(initButtons());
     }
 
-    //
+
     private Pane initButtons() {
         HBox box = new HBox();
         box.setAlignment(Pos.CENTER);
@@ -40,34 +45,23 @@ public class WeatherStationGUI extends BorderPane {
     private Pane initFields() {
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
-        grid.setPadding(new javafx.geometry.Insets(10, 10, 10, 10));
+        grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setHgap(20);
         grid.setVgap(2);
-        grid.add(new javafx.scene.control.Label("Time"), 1, 0);
+        grid.add(new Label("Time"), 1, 0);
         grid.add(time, 2, 0);
         time.setEditable(false);
-        grid.add(new javafx.scene.control.Label("Temp"), 1, 1);
+        grid.add(new Label("Temp"), 1, 1);
         grid.add(temperature, 2, 1);
         temperature.setEditable(false);
-        grid.add(new javafx.scene.control.Label("Pressure"), 1, 2);
+        grid.add(new Label("Pressure"), 1, 2);
         grid.add(pressure, 2, 2);
         pressure.setEditable(false);
-        grid.add(new javafx.scene.control.Label("Light"), 1, 3);
+        grid.add(new Label("Light"), 1, 3);
         grid.add(light, 2, 3);
         light.setEditable(false);
         return grid;
     }
-
-    private DAO getFieldData() {
-        DAO p = new DAO(DBmanager.getInstance());
-        p.getTime();
-        p.getTemp();
-        p.getPressure();
-        p.getLight();
-
-        return p;
-    }
-
 
     private void setFieldData(DAO db) {
         time.setText(String.valueOf(db.getTime()));
