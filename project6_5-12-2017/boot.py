db66f562c33bb7c5bdccf2e550a34d7de4ccfebc
mahmut <31988262+mahmut20@users.noreply.github.com>
2018-01-11 10:02:43 +0100

    Update boot.py

--- project6_5-12-2017/boot.py
+++ project6_5-12-2017/boot.py
@@ -12,6 +12,7 @@ si = SI7006A20()
 lt = LTR329ALS01()
 mp = MPL3115A2()
 
+#sla de gegevens op van ttn 
 lora = LoRa(mode=LoRa.LORAWAN)
 app_eui = binascii.unhexlify('70B3D57ED0008A37')
 app_key = binascii.unhexlify('A174D4D145F7ADB02A530F795E367CB7')
@@ -19,23 +20,23 @@ lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 
 
 while True:
-    print("\n")
+    #de temperatuur is gecapt tot minimaal -50 graden celcius
     temp=(round(si.temperature())+50)
-
+    # het licht varieert het meeste en hier heeft die bereik van 0 tot 64000 ongeveer
+    # wordt met lux gemeten
     light=round(lt.light())
-    light1= (int) ((light & 0xff0000)>>16)
     light2= (int) ((light & 0x00ff00)>>8)
     light3= (int) (light & 0x0000ff)
-
-    press=round(mp.pressure())
-    press1= (int) ((press & 0xff0000)>>16)
+    #het presure varieert meestal tussen 0.8... en 1.2.... het wordt met Pa gemeten
+    press=round(mp.pressure()-80000)
     press2= (int) ((press & 0x00ff00)>>8)
     press3= (int) (press & 0x0000ff)
 
     hum= round(si.humidity())
 
-    datas = bytes([temp, hum , light1,light2,light3, press1, press2, press3])
-
+    datas = bytes([temp, hum ,light2,light3, press2, press3])
+      
+    print("\n")
     print("temperature", (temp))
     print("hum",hum)
     print("light",light)
